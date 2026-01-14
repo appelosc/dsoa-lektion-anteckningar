e2dc26ae678e4f188f49c41a89586614ec82b9d4
appelosc <appelosc@arcada.fi>
2026-01-14 15:32:09 +0200

    Lektion 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        Car car = new Car("Porsche");
+        System.out.println(car.getName());
+        car.setName("VW");
+        System.out.println(car.getName());
     }
 }
