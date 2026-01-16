1422f6643d2a4812f04df4c04393a43d05620e60
appelosc <appelosc@arcada.fi>
2026-01-16 10:03:51 +0200

    Lektion 2 - superclasses

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lektion2.");
+        System.out.println("Hello");
+
+        Car car = new Car("Porsche");
+        Bike bike = new Bike("Scott");
+
+        System.out.println(car.getName() + " is " + car.getVehicleType() );
+        System.out.println(bike.getName() + " is " + bike.getVehicleType());
     }
-}
+    }
+
