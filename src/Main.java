eecd8a1b50ef6f240b48b8c047b9e7311b0c7483
appelosc <appelosc@arcada.fi>
2026-01-23 11:05:07 +0200

    CodeChallenge-Random Rundown

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        while(true){
+            Random random = new Random();
+            int rand = random.nextInt(1,99);
+            System.out.printf("Slumptal: %d\n",rand);
+            System.out.println("Q för att avsluta, annars fortsätter vi: ");
+            String textInput = scanner.nextLine();
+
+            if(textInput.equalsIgnoreCase("q")){
+                System.out.println("Tack o hej!");
+                break;
+            }
+        }
     }
 }
